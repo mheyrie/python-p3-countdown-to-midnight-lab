3ee73f0ac84019165d4c1734cf7bb10e708728f3
Mary <omota4fidelity@gmail.com>
2023-10-08 12:06:04 +0100

    done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,20 @@
 # your code goes here!
+import time
+
+def countdown(x):
+    while x > 0:
+        print(f"{x} SECOND(S)!")
+        x -= 1
+    print("HAPPY NEW YEAR!")
+
+countdown(10)
+
+def countdown_with_sleep(y):
+    while y > 0:
+        print(f"{y} SECOND(S)!")
+        time.sleep(1)
+        y -= 1
+        
+    print("HAPPY NEW YEAR!")
+
+countdown_with_sleep(5)
--- lib/debug.py
+++ lib/debug.py
@@ -2,5 +2,5 @@
 
 from countdown import countdown, countdown_with_sleep
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+# if __name__ == '__main__':
+#     import ipdb; ipdb.set_trace()
